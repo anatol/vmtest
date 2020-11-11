23c67c210a57d167db2d713d1e02ea58f93aae9e
Anatol Pomozov <anatol.pomozov@gmail.com>
2020-11-11 15:43:17 -0800

    Add ConsoleExpectRE() method to the VM interface

--- vm.go
+++ vm.go
@@ -1,7 +1,10 @@
 package vmtest
 
+import "regexp"
+
 type VM interface {
 	ConsoleExpect(str string) error
+	ConsoleExpectRE(re *regexp.Regexp) ([]string, error)
 	ConsoleWrite(str string) error
 	// Stop stops the current virtual machine
 	Stop()
